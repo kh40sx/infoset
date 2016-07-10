627842a1350f8f9fa8a5f18ce4897a99e9c68a78
Peter Harrison <blackriver@simiya.com>
2016-07-10 00:34:05 -0700

    Added ConfigCommon to __init__.py

--- infoset/utils/__init__.py
+++ infoset/utils/__init__.py
@@ -9,6 +9,7 @@ enough to warrant their own package
 from infoset.utils.jm_configuration import ConfigServer
 from infoset.utils.jm_configuration import ConfigAgent
 from infoset.utils.jm_configuration import ConfigSNMP
+from infoset.utils.jm_configuration import ConfigCommon
 from infoset.utils.xlate_snmp import Translator
 from infoset.utils.hidden import Directory
 from infoset.utils.hidden import File
