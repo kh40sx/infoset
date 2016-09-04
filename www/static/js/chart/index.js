041b4ff420e1696895950d536eb754965057e9b5
Pr0x1m4 <proxima.aust@gmail.com>
2016-09-03 19:28:38 -0700

    Multichart added

--- www/static/js/chart/index.js
+++ www/static/js/chart/index.js
@@ -1,4 +1,4 @@
-function AreaChart(uid, datapoint, target, width, height, fill) {
+function AreaChart(uid, datapoint, target, width, height, fill, title ,yLabel, start, stop) {
 	/*
 	Function for creating and appending charts to DOM
 
@@ -10,14 +10,13 @@ function AreaChart(uid, datapoint, target, width, height, fill) {
         Home Page
 	*/
 	//Create URL to request data
-	var dataroute = "/fetch/agent/graph/" + uid + "/" + datapoint;
+	var dataroute = "/fetch/agent/graph/" + uid + "/" + datapoint+ "?start=" + start +"&stop=" + stop;
 	var graph_width = width;
 	var graph_height = height;
 
-	var margin = {top: 20, right: 20, bottom: 30, left: 20},
+	var margin = {top: 40, right: 20, bottom: 30, left: 20},
 	    width = graph_width - margin.left - margin.right,
 	    height = graph_height - margin.top - margin.bottom;
-
 
 	//Scale x axis properly
 	var x = d3.time.scale()
@@ -41,6 +40,7 @@ function AreaChart(uid, datapoint, target, width, height, fill) {
 	    .orient("left")
 	    .innerTickSize(-width)
 	    .outerTickSize(0)
+	    .tickFormat(d3.format(".2s"))
 	    .tickPadding(10);
 
 	var line = d3.svg.line()
@@ -57,9 +57,19 @@ function AreaChart(uid, datapoint, target, width, height, fill) {
 	var svg = d3.select(target).append("svg")
 	    .attr("width", width + margin.left + margin.right)
 	    .attr("height", height + margin.top + margin.bottom)
+	    .style("padding-left", "70px")
+	    .style("padding-right", "70px")
 	    .attr("class", "center-block")
 	  	.append("g")
 	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
+	svg.append("text")
+        .attr("x", (width / 2))             
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px") 
+        .style("text-decoration", "underline")  
+        .text(title);
 
     d3.json(dataroute, function(error, data){
 
@@ -92,17 +102,18 @@ function AreaChart(uid, datapoint, target, width, height, fill) {
 		  .call(yAxis)
 		.append("text")
 		  .attr("transform", "rotate(-90)")
-		  .attr("y", 6)
-		  .attr("dy", ".71em")
+		  .attr("y", 8)
+		  .attr("dy", "-6em")
+		  .attr("dx", "-1.1em")
 		  .style("text-anchor", "end")
-		  .text("Data");
+		  .text(yLabel);
     });
 }
 
 function StackedArea(uid, datapoint, target, width, height, colors){
 
 	var dataroute = "/fetch/agent/graph/stacked/" + uid + "/" + datapoint;
-	var margin = {top: 20, right: 20, bottom: 30, left: 80},
+	var margin = {top: 20, right: 20, bottom: 30, left: 20},
 	    width = 630 - margin.left - margin.right,
 	    height = 240 - margin.top - margin.bottom;
 
@@ -117,15 +128,15 @@ function StackedArea(uid, datapoint, target, width, height, colors){
 	if (datapoint === "memory") {
 		colors = ['#71D5C3', '#009DB2', '#21D5C3', '#98e1d4', '#f0e0a0'];
 	} else if (datapoint === "load") {
-        colors = ['#F37372','#FA9469','#FDBB5D']
+        	colors = ['#F37372','#FA9469','#FDBB5D'];
 	} else if (datapoint === "network") {
-        colors = ['#000000','#00FFFF']
+        	colors = ['#DF6C5E','#4A89E6'];
 	} else if (datapoint === "cpu") {
-        colors = [
-            '#71D5C3', '#009DB2', '#21D5C3', '#98e1d4', '#f0e0a0',
-            '#FF0066', '#0000FF', '#009900', '#009999', '#FF9900',
-            '#660066', '#990000', '#000066', '#FFCC00', '#CC00FF'
-            ]
+		colors = [
+		    '#f0e0a0', '#f0e0a0', '#f0e0a0', '#98e1d4', '#f0e0a0',
+		    '#f0e0a0', '#0000FF', '#009900', '#009999', '#FF9900',
+		    '#660066', '#990000', '#000066', '#FFCC00', '#CC00FF'
+		    ];
 	}
 
 
@@ -141,7 +152,14 @@ function StackedArea(uid, datapoint, target, width, height, colors){
 	    .orient("left")
 	    .innerTickSize(-width)
 	    .outerTickSize(0)
+	    .tickFormat(d3.format("s"))
 	    .tickPadding(10);
+
+
+	var line = d3.svg.line()
+		.defined(function(d) {return d.y; })
+	    .x(function(d) { return x(d.x); })
+	    .y(function(d) { return y(d.y); });
 
 	var stack = d3.layout.stack()
 	    .offset("zero")
@@ -153,6 +171,7 @@ function StackedArea(uid, datapoint, target, width, height, colors){
 	    .key(function(d) { return d.group; });
 
 	var area = d3.svg.area()
+	    .defined(line.defined())
 	    .x(function(d) { return x(d.x); })
 	    .y0(function(d) { return y(d.y0); })
 	    .y1(function(d) { return y(d.y0 + d.y); });
@@ -160,6 +179,8 @@ function StackedArea(uid, datapoint, target, width, height, colors){
 	var svg = d3.select(target).append("svg")
 	    .attr("width", width + margin.left + margin.right)
 	    .attr("height", height + margin.top + margin.bottom)
+	    .style("padding-left", "40px")
+	    .style("padding-right", "40px")
 	  .append("g")
 	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -167,10 +188,6 @@ function StackedArea(uid, datapoint, target, width, height, colors){
 	  if (error) throw error;
 
 	  data.forEach(function(d) { d.x = new Date(d.x * 1000); });
-
-	  if (datapoint === "memory") {
-  		  data.forEach(function(d) { d.y = d.y / 10000000000; });
-	  }
 
 	  var layers = stack(nest.entries(data));
 
@@ -183,6 +200,90 @@ function StackedArea(uid, datapoint, target, width, height, colors){
 	      .attr("class", "layer")
 	      .attr("d", function(d) { return area(d.values); })
 	      .style("fill", function(d, i) { return colors[i]; })
+
+	  svg.append("g")
+	      .attr("class", "x axis")
+	      .attr("class", "grid")
+	      .attr("transform", "translate(0," + height + ")")
+	      .call(xAxis);
+
+	  svg.append("g")
+	      .attr("class", "y axis")
+  	      .attr("class", "grid")
+	      .call(yAxis);
+	
+	});
+}
+
+function MultiLineChart(uid, datapoint, target, width, height, colors){
+
+	var dataroute = "/fetch/agent/graph/stacked/" + uid + "/" + datapoint;
+	var colors = ["#71D59E", "#4a89e6"];
+	var margin = {top: 20, right: 20, bottom: 30, left: 20},
+	    width = 630 - margin.left - margin.right,
+	    height = 240 - margin.top - margin.bottom;
+
+	var x = d3.time.scale()
+	    .range([0, width]);
+
+	var y = d3.scale.linear()
+	    .range([height, 0]);
+
+	var z = d3.scale.category20c();
+
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom")
+	    .innerTickSize(-height)
+	    .outerTickSize(0)
+	    .tickPadding(10);
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left")
+	    .innerTickSize(-width)
+	    .outerTickSize(0)
+	    .tickFormat(d3.format("s"))
+	    .tickPadding(10);
+
+
+	var line = d3.svg.line()
+	    .defined(function(d) { return d.y; })
+	    .x(function(d) { return x(d.x); })
+	    .y(function(d) { return y(d.y); });
+
+	var nest = d3.nest()
+	    .key(function(d) { return d.group; });
+
+	var svg = d3.select(target).append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	    .style("padding-left", "40px")
+	    .style("padding-right", "40px")
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	d3.json(dataroute, function(error, data) {
+	  if (error) throw error;
+
+
+	  data.forEach(function(d) { d.x = new Date(d.x * 1000); });
+	  
+          x.domain(d3.extent(data, function(d) { return d.x; }));
+	  y.domain([0, d3.max(data, function(d) { return d.y; })]);
+	  
+
+	  var dataNest = nest.entries(data);
+	    dataNest.forEach(function(d, i) {
+		console.log(colors[i]);
+		svg.append("path")
+		    .attr("class", "line")
+		    .attr("d", line(d.values)) 
+		    .attr('stroke', colors[i])
+		    .attr('stroke-width', 1)
+		    .attr('fill', 'none');
+	    });
 
 	  svg.append("g")
 	      .attr("class", "x axis")
--- www/views.py
+++ www/views.py
@@ -10,7 +10,6 @@ import json
 import pprint
 import operator
 from os import path
-
 # Pip imports
 import yaml
 from flask import render_template, jsonify, send_file, request, make_response
@@ -220,7 +219,15 @@ def search_host(idx_host, idx_agent):
     data = sorted(data, key=operator.itemgetter(3, 2))
 
     # Render data on screen
-    return render_template('search-host.html', agent_list=data)
+    return render_template('search-host.html', idx_host=idx_host, agent_list=data)
+
+@infoset.route('/tables/<idx_host>/<idx_agent>')
+def tables(idx_host, idx_agent):
+    hostname = _infoset_hostname()
+    ip = '192.168.1.1'
+    return render_template('network-topo.html',
+                            hostname=hostname,
+                            ip=ip)
 
 
 @infoset.route('/hosts/<host>')
@@ -297,8 +304,8 @@ def layerTwo(host):
     return jsonify(layer2)
 
 
-@infoset.route('/graphs/<uid>/<idx>', methods=["GET", "POST"])
-def graphs(uid, idx):
+@infoset.route('/graphs/<idx_agent>/<idx_datapoint>', methods=["GET", "POST"])
+def graphs(idx_agent, idx_datapoint):
     """Create graphs.
 
     Args:
@@ -309,9 +316,19 @@ def graphs(uid, idx):
         None
 
     """
-    single_datapoint = db_datapoint.GetIDX(idx)
+    # Get agent details
+    agent_name = db_agent.GetIDX(idx_agent).name()
+    uid = db_agent.GetIDX(idx_agent).uid()
+
+    agent_source = str(request.args.get('source'))
+
+    # Get a description of the datapoint
+    lang = language.Agent(agent_name)
+    
+    single_datapoint = db_datapoint.GetIDX(idx_datapoint)
     agent_label = single_datapoint.agent_label()
 
+    agent_description = lang.label_description(agent_label)
     colorwheel = ColorWheel(agent_label)
     fill = colorwheel.get_scheme()
     preset = TimeStamp()
@@ -319,8 +336,11 @@ def graphs(uid, idx):
     return render_template('graphs.html',
                            timestamps=timestamps,
                            uid=uid,
-                           datapoint=idx,
-                           fill=fill)
+                           datapoint=idx_datapoint,
+                           fill=fill,
+                           description=agent_description,
+                           agent_source=agent_name)
+
 
 
 @infoset.route('/receive/<uid>', methods=["POST"])
@@ -438,10 +458,6 @@ def fetch_graph_stacked(uid, stack_type):
 
     # Define label values
     labels['cpu'] = [
-        'cpu_times_percent_user',
-        'cpu_times_percent_nice',
-        'cpu_times_percent_system',
-        'cpu_times_percent_idle',
         'cpu_times_percent_iowait',
         'cpu_times_percent_irq',
         'cpu_times_percent_ctx_switches',
@@ -450,6 +466,10 @@ def fetch_graph_stacked(uid, stack_type):
         'cpu_times_percent_soft_interrupts',
         'cpu_times_percent_softirq',
         'cpu_times_percent_steal',
+        'cpu_times_percent_user',
+        'cpu_times_percent_nice',
+        'cpu_times_percent_system',
+        'cpu_times_percent_idle',
         'cpu_times_percent_guest',
         'cpu_times_percent_guest_nice'
     ]
@@ -504,7 +524,7 @@ def fetch_graph_stacked(uid, stack_type):
     return jsonify(values)
 
 
-@infoset.route('/fetch/agent/<ip>/table', methods=["GET"])
+@infoset.route('/fetch/agent/<ip_address>/table', methods=["GET"])
 def fetch_table(ip_address):
     """Return Network Layout tables.
 
@@ -548,7 +568,6 @@ def _datapoint_labels(idx_host, idx_agent, labels):
 
     # Return
     return listing
-
 
 def _infoset_hostname():
     """Get hostname for _infoset agent.
